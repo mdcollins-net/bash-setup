c1eda341c665e29d63a2019f2b0240acc6465928
Mark Collins <mark@mdcollins.net>
2020-03-05 16:57:25 +0100

    Updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# To run from remote:
+#
+# wget -q http://bit.ly/mcbashsetup -O -
+#
+
+
 banner="
 ___  ____ ____ _  _        ____ ____ ___ _  _ ___
 |__] |__| [__  |__|   __   [__  |___  |  |  | |__]
@@ -10,9 +17,10 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 packages="curl dos2unix fasd linuxbrew-wrapper python3.8 golang-go"
-node_packages="@aweary/alder git-commander fkill-cli"
 python_packages="speedtest-cli"
-
+go_packages="github.com/justjanne/powerline-go"
+node_packages="@aweary/alder git-commander fkill-cli"
+rust_packages="exa tre"
 
 print_banner () {
   echo -e "${bold}${banner}${normal}"
@@ -34,7 +42,7 @@ install_packages () {
 }
 
 install_node () {
-  echo -e "\n${bold}Installing node.js ... ${normal}\n"
+  echo -e "\n${bold}Installing Node.js ... ${normal}\n"
   curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
   sudo apt-get install -y nodejs
 
@@ -44,9 +52,9 @@ install_node () {
   sudo apt-get update && sudo apt-get install yarn
 
   echo -e "\nInstalling / Updating ${bold}npm${normal} ... \n"
-  npm install -g npm
+  sudo -H npm install -g npm
 
-  echo -e "\n${bold}Finished installing node.js ... ${normal}\n"
+  echo -e "\n${bold}Finished installing Node.js ... ${normal}\n"
 }
 
 install_node_package () {
@@ -55,32 +63,80 @@ install_node_package () {
 }
 
 install_node_packages () {
-  echo -e "\n${bold}Installing Node.js packages ... ${normal}\n"
+  echo -e "\n${bold}Installing Node packages ... ${normal}\n"
   for p in ${node_packages}; do
     install_node_package "${p}"
   done
-  echo -e "\n${bold}Finished Node.js installing packages. ${normal}\n"
+  echo -e "\n${bold}Finished installing Node packages. ${normal}\n"
 }
 
 install_python_package () {
   echo -e "\nInstalling Python package: ${bold}${1}${normal} ... \n"
-  python -m pip install "${1}"
+  sudo -H python -m pip install "${1}"
 }
 
 install_python_packages () {
   echo -e "\n${bold}Installing Python packages ... ${normal}\n"
+  echo -e "\nInstalling / Updating ${bold}pip${normal} ... \n"
+  sudo -H python -m pip install -U pip
   for p in ${python_packages}; do
     install_python_package "${p}"
   done
-  echo -e "\n${bold}Finished Python installing packages. ${normal}\n"
+  echo -e "\n${bold}Finished installing Python packages. ${normal}\n"
+}
+
+install_go_package () {
+  echo -e "\nInstalling Go package: ${bold}${1}${normal} ... \n"
+  go get -u -v  "${1}"
+}
+
+install_go_packages () {
+  echo -e "\n${bold}Installing Go packages ... ${normal}\n"
+  for p in ${go_packages}; do
+    install_go_package "${p}"
+  done
+  echo -e "\n${bold}Finished installing Go packages. ${normal}\n"
 }
 
 
+install_rust () {
+  echo -e "\n${bold}Installing Rust ... ${normal}\n"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+  source "~"/.cargo/env
+  echo -e "\n${bold}Finished installing Rust ... ${normal}\n"
+}
+
+install_rust_package () {
+  echo -e "\nInstalling Rust package: ${bold}${1}${normal} ... \n"
+  cargo install "${p}"
+}
+
+install_rust_packages () {
+  echo -e "\n${bold}Installing Rust packages ... ${normal}\n"
+  for p in ${rust_packages}; do
+    install_rust_package "${p}"
+  done
+  echo -e "\n${bold}Finished installing Rust packages. ${normal}\n"
+}
+
+create_directories () {
+  cd ${HOME}
+  mkdir -p backup/{ssh,profile}
+}
+
 print_banner
+
 install_packages
+install_python_packages
+install_go_packages
+
 install_node
 install_node_packages
-install_python_packages
+
+install_rust
+install_rust_packages
+
+create_directories
 
 
 
