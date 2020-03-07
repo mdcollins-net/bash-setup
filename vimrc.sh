675bc0dbd680ee3c1c9020e0e3d7cce40ad63ad6
Mark Collins <mark@mdcollins.net>
2020-03-07 21:23:57 +0100

    Updates

--- vimrc.sh
+++ vimrc.sh
@@ -7,7 +7,10 @@ set autoindent
 set smartindent
 set ruler
 set tabstop=4
+set softtabstop=4
 set shiftwidth=4
+set textwidth=79
+set expandtab
 set visualbell
 set expandtab
 set t_vb=
@@ -50,18 +53,3 @@ let g:gitgutter_override_sign_column_highlight = 1
 
 highlight SignColumn guibg=bg
 highlight SignColumn ctermbg=bg
-
-au BufNewFile,BufRead *.py
-    \ set tabstop=4
-    \ set softtabstop=4
-    \ set shiftwidth=4
-    \ set textwidth=79
-    \ set expandtab
-    \ set autoindent
-    \ set fileformat=unix
-
-au BufNewFile,BufRead *.java, *.js, *.html, *.css
-    \ set tabstop=4
-    \ set softtabstop=4
-    \ set shiftwidth=4
-
