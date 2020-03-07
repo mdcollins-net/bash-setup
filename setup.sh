c7708a636388c92d3420e2ce85f225574b064bee
Mark Collins <mark@mdcollins.net>
2020-03-07 21:08:18 +0100

    Updates

--- setup.sh
+++ setup.sh
@@ -192,7 +192,7 @@ make_backups () {
 vim_setup_color_themes () {
   echo -e "Downloading VIM themes ... \n"
   # VIM Theme : Zenburn
-  wget -O "${HOME}/.vim/colors/zenburn.vim" ${URL_VIM_THEME_ZENBURN}
+  wget -O --no-cache "${HOME}/.vim/colors/zenburn.vim" ${URL_VIM_THEME_ZENBURN}
   dos2unix "${HOME}/.vim/colors/zenburn.vim"
   echo -e "Finished downloading VIM themes.\n"
 }
@@ -200,7 +200,7 @@ vim_setup_color_themes () {
 vim_setup_plugins () {
   echo -e "Downloading VIM plugins ... \n"
   # VIM Plugin : Plug
-  wget -O "${HOME}/.vim/autoload/plug.vim" ${URL_VIM_PLUGIN_PLUG}
+  wget -O --no-cache "${HOME}/.vim/autoload/plug.vim" ${URL_VIM_PLUGIN_PLUG}
   dos2unix "${HOME}/.vim/autoload/plug.vim"
   echo -e "Finished downloading VIM plugins.\n"
 }
@@ -223,7 +223,7 @@ vim_setup () {
   vim_setup_plugins
 
   echo -e "Downloading vimrc ... \n"
-  wget -O "${TEMP_DIR}/vimrc" ${URL_VIM_VIMRC}
+  wget -O --no-cache "${TEMP_DIR}/vimrc" ${URL_VIM_VIMRC}
   dos2unix "${TEMP_DIR}/vimrc"
   cp "${TEMP_DIR}/vimrc" "${HOME}/.vimrc"
 
@@ -233,7 +233,7 @@ vim_setup () {
 profile_setup () {
   echo -e "\n${BOLD}Setting up BASH profile ... ${NORMAL}\n"
 
-  wget -O "${TEMP_DIR}/profile" ${URL_PROFILE}
+  wget -O --no-cache "${TEMP_DIR}/profile" ${URL_PROFILE}
   dos2unix "${TEMP_DIR}/profile"
   cp "${TEMP_DIR}/profile" "${HOME}/.profile"
 
